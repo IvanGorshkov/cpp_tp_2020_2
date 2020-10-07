d1fbe9800fd3038033899a7d471d1858ac80126a
Ivan Gorshkov <ivan.a.gorshkov@gmail.com>
2020-10-07 11:39:01 +0300

    Убрал лишние пробелф

--- project/src/matrix.c
+++ project/src/matrix.c
@@ -14,32 +14,31 @@ Matrix* create_empty_matrix(size_t rows, size_t cols) {
     if (rows == 0 || cols == 0) {
         return NULL;
     }
-    
+
     Matrix *matrix = calloc(1, sizeof(Matrix));
-    
+
     if (matrix == NULL) {
         return NULL;
     }
-    
+
     matrix->rows = rows;
     matrix->cols = cols;
     matrix->matrix = calloc(rows * cols, sizeof(double));
-    
+
     if (matrix->matrix == NULL) {
         free_matrix(matrix);
         return NULL;
     }
-    
+
     return matrix;
 }
 
 Matrix* generate_adjacency(double *old_matrix, size_t array_rows, size_t array_cols) {
-    Matrix *new_matrix = create_empty_matrix(array_rows,array_cols);
-    
+    Matrix *new_matrix = create_empty_matrix(array_rows, array_cols);
     if (new_matrix == NULL) {
         return NULL;
     }
-    
+
     for (size_t rows = 0; rows < array_rows; ++rows) {
         double sum = 0;
         int middle = 0;
@@ -49,59 +48,59 @@ Matrix* generate_adjacency(double *old_matrix, size_t array_rows, size_t array_c
                 sum += old_matrix[new_matrix->rows * (cols - 1) + rows];
                 middle += 2;
             }
-            
+
             if (rows < (array_rows- 1) && rows > 0) {
                 sum += old_matrix[new_matrix->rows * cols + (rows - 1)];
                 sum += old_matrix[new_matrix->rows * cols + (rows + 1)];
                 middle += 2;
             }
-            
+
             if (rows == 0 && array_rows != 1) {
                 sum += old_matrix[new_matrix->rows * cols + (rows + 1)];
                 ++middle;
             }
-            
+
             if (rows == (array_rows - 1) && rows != 0) {
                 sum += old_matrix[new_matrix->rows * cols + (rows - 1)];
                 ++middle;
             }
-            
+
             if (cols == 0 && array_cols != 1) {
                 sum += old_matrix[new_matrix->rows * (cols + 1) + rows];
                 ++middle;
             }
-            
+
             if (cols == (array_cols - 1) && cols != 0) {
                 sum += old_matrix[new_matrix->rows * (cols - 1) + rows];
                 ++middle;
             }
-            
+
             if (cols != (array_cols - 1) && rows != (array_rows - 1)) {
                 sum += old_matrix[new_matrix->rows * (cols + 1) + (rows + 1)];
                 ++middle;
             }
-            
+
             if (cols != 0 && rows != 0) {
                 sum += old_matrix[new_matrix->rows * (cols - 1) + (rows - 1)];
                 ++middle;
             }
-            
+
             if (rows != 0 && cols != (array_cols - 1)) {
                 sum += old_matrix[new_matrix->rows * (cols + 1) + (rows - 1)];
                 ++middle;
             }
-            
+
             if (rows != (array_rows - 1) && cols != 0) {
                 sum += old_matrix[new_matrix->rows * (cols - 1) + (rows + 1)];
                 ++middle;
             }
-            
+
             new_matrix->matrix[new_matrix->rows * cols + rows] = sum / middle;
             sum = 0;
             middle = 0;
         }
     }
-    
+
     return new_matrix;
 }
 
@@ -109,7 +108,7 @@ int print_matrix(Matrix *matrix) {
     if (matrix == NULL) {
         return EXIT_FAILURE;
     }
-    
+
     for (size_t rows = 0; rows < matrix->rows; ++rows) {
         for (size_t cols = 0; cols < matrix->cols; ++cols) {
             printf("%f ", matrix->matrix[matrix->cols * rows + cols]);
@@ -124,14 +123,14 @@ int free_matrix(Matrix *matrix) {
     if (matrix == NULL) {
         return EXIT_FAILURE;
     }
-    
+
     if (matrix->matrix == NULL) {
         free(matrix);
         return EXIT_FAILURE;
     }
-    
+
     free(matrix->matrix);
     free(matrix);
-    
+
     return EXIT_SUCCESS;
 }
--- project/src/utils.c
+++ project/src/utils.c
@@ -15,16 +15,16 @@ int convert_str_to_arr(const char* str, const unsigned long size, double **linem
     if (arr == NULL) {
         return EXIT_FAILURE;
     }
-    
+
     int num_array = 0;
     for (int num_letter = 0; str[num_letter] != '\0'; ++num_letter) {
         if (str[num_letter] != ' ') {
-            if( str[num_letter] < '0' || str[num_letter] > '9' ) {
+            if (str[num_letter] < '0' || str[num_letter] > '9' ) {
                 free(arr);
                 arr = NULL;
                 return EXIT_FAILURE;
             }
-            
+
             if (num_array < size) {
                 arr[num_array] = arr[num_array] * 10 + (str[num_letter] - 48);
             } else {
@@ -35,7 +35,7 @@ int convert_str_to_arr(const char* str, const unsigned long size, double **linem
             num_array++;
         }
     }
-    
+
     if (arr == NULL) {
         return EXIT_FAILURE;
     }
