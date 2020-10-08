dc4094c93782176919b3e6154c39732277a2b39f
Ivan Gorshkov <ivan.a.gorshkov@gmail.com>
2020-10-08 12:10:17 +0300

    Пытаюсь сделать тест

---
+++ project/tests/include/matrix_utils.hpp
@@ -0,0 +1,21 @@
+//
+//  matrix_utils.hpp
+//  DZ1
+//
+//  Created by Ivan Gorshkov on 08.10.2020.
+//
+
+#ifndef matrix_utils_hpp
+#define matrix_utils_hpp
+
+#include <stdio.h>
+#include <experimental/filesystem>
+#include "/project/include/matrix.h"
+#include <gtest/gtest.h>
+#include <iostream>
+#include <fstream>
+
+namespace fs = std::experimental::filesystem;
+extern fs::path matrixDataPath;
+
+#endif /* matrix_utils_hpp */
---
+++ project/tests/src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DZ1
+//
+//  Created by Ivan Gorshkov on 08.10.2020.
+//
+
+#include <stdio.h>
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
