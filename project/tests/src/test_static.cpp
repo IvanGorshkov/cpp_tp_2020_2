87dfaaf9e3583b213ad9990215b618642105c6a4
Ivan Gorshkov <ivan.a.gorshkov@gmail.com>
2020-10-22 13:40:36 +0300

    mem leaks

--- project/tests/src/test_static.cpp
+++ project/tests/src/test_static.cpp
@@ -7,21 +7,3 @@
 extern "C" {
   #include "libstat.h"
 }
-
-TEST(StaticLib, GetLineSizeFromFileStat) {
-  int i = 1;
-  for (auto& entry : fs::directory_iterator(glob_test_dir / "data")) {
-    std::cout << entry.path().parent_path() << std::endl;
-    std::string path = entry.path().parent_path();
-    path += "/out_" + std::to_string(i) + ".txt";
-    std::ifstream is(path);
-    int expected_det = 0;
-    is >> expected_det;
-    is.close();
-    path = entry.path().parent_path();
-    path += "/in_" + std::to_string(i) + ".txt";
-    ASSERT_EQ(expected_det, sequential_get_size_of_lines(path.c_str()));
-    if (i == number_of_files) break;
-    ++i;
-  }
-}
