e2ba6fc05e2ce28feb02786a197900e7c8959adc
Ivan Gorshkov <ivan.a.gorshkov@gmail.com>
2020-10-22 13:34:12 +0300

    mem leaks

--- project/tests/src/test_dyn.cpp
+++ project/tests/src/test_dyn.cpp
@@ -8,22 +8,3 @@ extern "C" {
   #include "libdyn.h"
 }
 
-TEST(ParallelLib, GetLineSizeFromFileDyn) {
-    int i = 1;
-    for (auto& entry : fs::directory_iterator(glob_test_dir / "data")) {
-      std::cout << entry.path().parent_path() << std::endl;
-      std::string path = entry.path().parent_path();
-      path += "/out_" + std::to_string(i) + ".txt";
-      std::ifstream is(path);
-      int expected_det = 0;
-      is >> expected_det;
-      is.close();
-      path = entry.path().parent_path();
-      path += "/in_" + std::to_string(i) + ".txt";
-      std::cout << path << std::endl;
-      ASSERT_EQ(expected_det, parallel_get_size_of_lines(path.c_str()));
-      if (i == number_of_files)
-        break;
-      ++i;
-    }
-}
